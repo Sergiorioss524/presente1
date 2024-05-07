75dd4153ad52ef2884ab8387e40df23d4d239338
sergio <riosgenuziosergio@gmail.com>
2024-05-07 15:11:08 +0200

    commit

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,9 @@
+const headerNavLinks = [
+  { href: '/', title: 'Home' },
+  { href: '/blog', title: 'Blog' },
+  { href: '/tags', title: 'Tags' },
+  { href: '/projects', title: 'Algo' },
+  { href: '/about', title: 'Nosotros' },
+]
+
+export default headerNavLinks
