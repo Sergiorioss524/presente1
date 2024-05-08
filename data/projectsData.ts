412067b88002d1bb418305ef1057f3f79e8ee1c9
sergio <riosgenuziosergio@gmail.com>
2024-05-08 19:42:46 +0200

    noticias + ejemplo

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,20 +7,16 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Evo Morales en el MAS',
+    description: `Evo Morales fue reemplazado como líder del MAS en Bolivia por Grover García, señalando un cambio hacia la unidad y transparencia.`,
+    imgSrc: '/static/images/evoLucho.png',
+    href: '/blog/ejemplo',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Dolares en Bolivia',
+    description: `Los bonos en dólares fueron lanzados por el Banco Central de Bolivia como parte del acuerdo al que llegó el Gobierno con los empresarios.`,
+    imgSrc: '/static/images/dolares.png',
+    href: '/blog/dolares',
   },
 ]
 
