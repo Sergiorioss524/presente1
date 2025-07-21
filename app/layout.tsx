7c353e6c09d5103c9df5d4b052da4f750d428d6e
sergio <riosgenuziosergio@gmail.com>
2025-07-21 18:01:18 +0200

    google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -75,6 +75,17 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-P65GH48QDF"></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-P65GH48QDF');
+`,
+        }}
+      />
       <body className="bg-white pl-[calc(100vw-100%)] text-black antialiased dark:bg-gray-950 dark:text-white">
         <ThemeProviders>
           <Analytics />
