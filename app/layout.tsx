f9c7a94e4a583b3382624d92587e09fe18094562
sergio <riosgenuziosergio@gmail.com>
2026-03-22 17:59:58 -0400

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,11 +67,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       className={`${rubik.variable} scroll-smooth`}
       suppressHydrationWarning
     >
-      <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/favicon.png" />
+      <link rel="icon" type="image/svg+xml" href="/static/favicons/favicon.svg" />
       <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon.png" />
+      <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/favicon.png" />
       <link rel="manifest" href="/static/favicons/site.webmanifest" />
-      <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+      <link rel="mask-icon" href="/static/favicons/favicon.svg" color="#5bbad5" />
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
